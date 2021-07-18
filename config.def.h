4eff22a389dcaeefa7a9ab87b8dcaa41a05a198c
mergen <mergenxu@gmail.com>
2021-07-18 14:41:27 +0800

    Ligatures

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "PragmataPro Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "PragmataPro Liga:pixelsize=20:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Liberation Mono:pixelsize=12:antialias=true:autohint=true",
