55e57d9da4f4ad543bf5a75b67c8ef0d13fb1392
mergenxu <mergenxu@gmail.com>
2021-07-22 10:03:47 +0800

    init

--- config.def.h
+++ config.def.h
@@ -5,11 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "PragmataPro Liga:pixelsize=20:antialias=true:autohint=true";
+static char *font = "PragmataPro Liga:pixelsize=21:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Liberation Mono:pixelsize=12:antialias=true:autohint=true",
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Noto Sans Mono CJK SC:pixelsize=20:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
